5fa942522c89161ad6fdaff3397a1e9713644387
johnny macmillan <jpmac67@icloud.com>
2026-09-06 19:20:47 +0100

    Blueprints for the array/algos
    also used namespaces to make code nicer, along with that using classes to help code feel more organized, going to have dinner then do more work on it after

--- visualizer.cpp
+++ visualizer.cpp
@@ -1,20 +1,47 @@
 #include "raylib.h"
+#include <vector>
+#include <random>
+
+using namespace std;
+class AlgoVisualizer {
+public: 
+	AlgoVisualizer() {
+		InitWindow(WIDTH, HEIGHT, "VISUALIZER");
+		SetTargetFPS(60);
+
+		mt19937 gen{};
+		uniform_real_distribution<float> range{ 0.0f, 1.0f };
+	
+		for (size_t i{}; i < MAX_AMOUNT; i++) {
+			m_to_sort.push_back(range(gen));
+		}
+	}
+	void main() {
+		while(!WindowShouldClose())
+		{
+			PollInputEvents();
 
 
-int main() {
-	InitWindow(800, 600, "window");
-	SetTargetFPS(240);
+			BeginDrawing();
+			EndDrawing();
 
-	while (!WindowShouldClose()) {
-
-		BeginDrawing();
-		ClearBackground(RAYWHITE);
-		DrawText("raylib #working #beastmode", 100, 250, 40, DARKGRAY);
-		EndDrawing();
+			ClearBackground(BLACK);
+		}
+		
 	}
 
+private: 
 	
-	CloseWindow();
+	const int WIDTH{ 1600};
+	const int HEIGHT{ 800 };
+	const int MAX_AMOUNT{ 100 };
+	vector<float> m_to_sort{};
+};
 
-	return 0;
-}
+	int main() {
+		AlgoVisualizer instance{};
+		instance.main();
+
+		return 0;
+	}
+
